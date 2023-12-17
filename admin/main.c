717fb45cd5b7aa3b11a3a0192bbd8634233dc0bf
OneAimlessRobot <adri.campos.valente@gmail.com>
2023-12-17 22:01:38 +0000

    .randStringGenerator/randStringGenerator 10

---
+++ admin/main.c
@@ -0,0 +1,317 @@
+#include "Includes/preprocessor.h"
+static const char* ping= "gimmemore!";
+
+static int alive=1;
+#define MAXNUMBEROFTRIES 10
+
+#define MAXTIMEOUTSECS 1
+#define MAXTIMEOUTUSECS 0
+
+#define MAXTIMEOUTCMD (60*5)
+#define MAXTIMEOUTUCMD 0
+
+#define MAXTIMEOUTCONS (60*5)
+#define MAXTIMEOUTUCONS 0
+
+#define MAXTIMEOUTPING 1
+#define MAXTIMEOUTUPING 0
+
+#define LINESIZE 1024
+#define ARGVMAX 100
+
+#define DATASIZE 1024
+
+static pthread_mutex_t varMtx= PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t outMtx= PTHREAD_MUTEX_INITIALIZER;
+static pthread_t connectionChecker;
+static pthread_t outputWritter;
+static pthread_t errWritter;
+int outpipe[2],errpipe[2];
+int server_socket,client_socket,output_socket,lifeline_socket,err_socket;
+char line[LINESIZE]={0};
+char  outbuff[DATASIZE]={0};
+char  errbuff[DATASIZE]={0};
+struct sockaddr_in server_address;
+	
+static void sigint_handler(int signal){
+
+        acessVarMtx(&varMtx,&alive,0,0);
+        pthread_join(connectionChecker,NULL);
+	pthread_join(outputWritter,NULL);
+	pthread_join(errWritter,NULL);
+	close(server_socket);
+	close(client_socket);
+        close(lifeline_socket);
+        close(output_socket);
+	close(err_socket);
+	close(outpipe[0]);
+	close(outpipe[1]);
+	close(errpipe[0]);
+	close(errpipe[1]);
+	fflush(stdout);
+	fflush(stderr);
+	close(1);
+	close(2);
+	exit(-1);
+}
+
+static void sigpipe_handler(int signal){
+
+	raise(SIGINT);
+}
+
+
+static void initpipes(void){
+
+if(pipe(outpipe)){
+
+perror("Erro a criar pipe de stdout!!!\n");
+raise(SIGINT);
+}
+dup2(outpipe[1],1);
+long flags=fcntl(outpipe[0],F_GETFL);
+flags |=O_NONBLOCK;
+fcntl(outpipe[0],F_SETFL,flags);
+
+if(pipe(errpipe)){
+
+perror("Erro a criar pipe de stderr!!!\n");
+raise(SIGINT);
+}
+dup2(errpipe[1],2);
+flags=fcntl(errpipe[0],F_GETFL);
+flags |=O_NONBLOCK;
+fcntl(errpipe[0],F_SETFL,flags);
+
+}
+static void acceptConnection(int* socket){
+
+		int iResult;
+                struct timeval tv;
+                fd_set rfds;
+                FD_ZERO(&rfds);
+                FD_SET(server_socket,&rfds);
+                tv.tv_sec=MAXTIMEOUTCONS;
+                tv.tv_usec=MAXTIMEOUTUCONS;
+                iResult=select(server_socket+1,&rfds,(fd_set*)0,(fd_set*)0,&tv);
+        if(iResult>0){
+ 
+		*socket=accept(server_socket,NULL,NULL);
+	}
+	else{
+		raise(SIGINT);
+	}
+	printf("Coneccao de %s!!!!!!\n",inet_ntoa(server_address.sin_addr));
+	
+}
+static void initServer(int port){
+
+
+	
+	server_socket= socket(AF_INET,SOCK_STREAM,0);
+	if(server_socket==-1){
+		raise(SIGINT);
+	}
+	signal(SIGINT,sigint_handler);
+	signal(SIGPIPE,sigpipe_handler);
+	fcntl(server_socket,F_SETFL,O_NONBLOCK);
+	server_address.sin_family=AF_INET;
+	server_address.sin_port= htons(port);
+	server_address.sin_addr.s_addr = INADDR_ANY;
+	socklen_t len;
+	
+	bind(server_socket,(struct sockaddr*) &server_address,sizeof(server_address));
+
+        listen(server_socket,3);
+
+}
+static int64_t receiveClientInput(int socket,char buff[],u_int64_t size,int secwait,int usecwait){
+                int iResult;
+                struct timeval tv;
+                fd_set rfds;
+   	        FD_ZERO(&rfds);
+                FD_SET(socket,&rfds);
+                tv.tv_sec=secwait;
+                tv.tv_usec=usecwait;
+		iResult=select(socket+1,&rfds,(fd_set*)0,(fd_set*)0,&tv);
+                if(iResult>0){
+                return recv(socket,buff,size,0);
+                }
+                return -1;
+}
+static int64_t timedRead(int fd,char buff[],u_int64_t size,int secwait,int usecwait){
+                int iResult;
+                struct timeval tv;
+                fd_set rfds;
+   	        FD_ZERO(&rfds);
+                FD_SET(fd,&rfds);
+                tv.tv_sec=secwait;
+                tv.tv_usec=usecwait;
+		iResult=select(fd+1,&rfds,(fd_set*)0,(fd_set*)0,&tv);
+                if(iResult>0){
+                return read(fd,buff,size);
+                }
+                return -1;
+}
+
+static int makeargv(char *s, char *argv[ARGVMAX]) {
+    int ntokens = 0;
+
+    if (s == NULL || argv == NULL || ARGVMAX == 0)
+        return -1;
+    argv[ntokens] = strtok(s, " \t\n");
+    while ((argv[ntokens] != NULL) && (ntokens < ARGVMAX)) {
+        ntokens++;
+        argv[ntokens] = strtok(NULL, " \t\n");
+    }
+    argv[ntokens] = NULL; // it must terminate with NULL
+    return ntokens;
+}
+
+static void* areYouStillThere(void* args){
+	
+        int pingLength=strlen(ping);
+	char buff[strlen(ping)];
+        memset(buff,0,pingLength);
+	
+	receiveClientInput(lifeline_socket,buff,pingLength,MAXTIMEOUTPING,MAXTIMEOUTUPING);
+	while(acessVarMtx(&varMtx,&alive,0,-1)){
+
+		send(lifeline_socket,ping,strlen(ping),0);
+		int status=receiveClientInput(lifeline_socket,buff,strlen(ping),MAXTIMEOUTPING,MAXTIMEOUTUPING);
+                if(status<0){
+                        raise(SIGINT);
+                }
+		memset(buff,0,pingLength);
+
+
+
+	}
+	return NULL;
+
+}
+static void* writeOutput(void* args){
+	while(acessVarMtx(&varMtx,&alive,0,-1)){
+		char buff[strlen(ping)];
+		int numread=1;
+		while((numread=timedRead(outpipe[0],outbuff,DATASIZE,MAXTIMEOUTSECS,MAXTIMEOUTUSECS))>0){
+		send(output_socket,outbuff,DATASIZE,0);
+		receiveClientInput(output_socket,buff,sizeof(buff),MAXTIMEOUTSECS,MAXTIMEOUTUSECS);
+		memset(outbuff,0,DATASIZE);
+		}
+		
+		
+
+	}
+	return NULL;
+
+
+
+}
+static void* writeErr(void* args){
+	while(acessVarMtx(&varMtx,&alive,0,-1)){
+		char buff[strlen(ping)];
+		int numread=1;
+		
+		while((numread=timedRead(errpipe[0],errbuff,DATASIZE,MAXTIMEOUTSECS,MAXTIMEOUTUSECS))>0){
+		send(err_socket,errbuff,DATASIZE,0);
+		receiveClientInput(err_socket,buff,sizeof(buff),MAXTIMEOUTSECS,MAXTIMEOUTUSECS);
+		memset(errbuff,0,DATASIZE);
+		}
+		
+
+	}
+	return NULL;
+
+
+
+}
+void setupConnections(void){
+
+
+	acceptConnection(&client_socket);
+ 
+	acceptConnection(&lifeline_socket);
+	
+	acceptConnection(&output_socket);
+ 	
+	acceptConnection(&err_socket);
+ 	long flags= fcntl(client_socket,F_GETFL);
+	flags |= O_NONBLOCK|O_ASYNC;
+        fcntl(client_socket,F_SETFD,flags);
+	
+	flags= fcntl(lifeline_socket,F_GETFL);
+	flags |= O_NONBLOCK;
+        fcntl(lifeline_socket,F_SETFD,flags);
+	
+	flags= fcntl(output_socket,F_GETFL);
+	flags |= O_NONBLOCK;
+        fcntl(output_socket,F_SETFD,flags);
+
+	flags= fcntl(err_socket,F_GETFL);
+	flags |= O_NONBLOCK;
+        fcntl(err_socket,F_SETFD,flags);
+	
+	char sizes[10]={0};
+	snprintf(sizes,10,"%hu",DATASIZE);
+	send(client_socket,sizes,10,0);
+	char buff[strlen(ping)];
+	memset(buff,0,strlen(ping));
+	receiveClientInput(client_socket,buff,strlen(ping),MAXTIMEOUTCONS,MAXTIMEOUTUCONS);
+	
+
+}
+int main(int argc, char ** argv){
+	
+	if(argc!=2){
+
+		printf("arg1: porta do server\n");
+		exit(-1);
+	}
+
+	initServer(atoi(argv[1]));
+	
+	setupConnections();
+
+	initpipes();
+	
+	pthread_create(&connectionChecker,NULL,areYouStillThere,NULL);
+	
+	pthread_create(&outputWritter,NULL,writeOutput,NULL);
+
+	pthread_create(&errWritter,NULL,writeErr,NULL);
+
+	//receber e armazenar dados recebidos
+        
+	
+	
+	while(acessVarMtx(&varMtx,&alive,0,-1)){
+	//perror("Gormitis de fogo\n");
+	while(receiveClientInput(client_socket,line,LINESIZE,MAXTIMEOUTCMD,MAXTIMEOUTUCMD)>0){
+	char buff[strlen(ping)+1];
+	memset(buff,0,strlen(ping)+1);
+	memcpy(buff,ping,strlen(ping));
+	
+	char* args[ARGVMAX]={0};
+	makeargv(line,args);
+	fflush(stdout);
+	fflush(stderr);
+	int pid= fork();
+	switch(pid){
+		case -1:
+			exit(-1);
+		case 0:
+			execvp(args[0], args);
+			exit(-1);
+		default:
+			while(!wait(NULL));
+		break;
+
+	}
+	send(client_socket,buff,sizeof(buff),0);	
+	}
+	}
+	raise(SIGINT);
+	return 0;
+}
+
---
+++ client/main.c
@@ -0,0 +1,264 @@
+#include "Includes/preprocessor.h"
+static const char* ping= "gimmemore!";
+
+static int alive=1;
+u_int16_t dataSize;
+#define MAXNUMBEROFTRIES 10
+
+#define MAXTIMEOUTSECS 1
+#define MAXTIMEOUTUSECS 0
+
+#define MAXTIMEOUTCMD (60*5)
+#define MAXTIMEOUTUCMD 0
+
+#define MAXTIMEOUTCONS (60*5)
+#define MAXTIMEOUTUCONS 0
+
+#define MAXTIMEOUTPING 1
+#define MAXTIMEOUTUPING 0
+
+#define LINESIZE 1024
+
+
+static pthread_mutex_t varMtx= PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t readyCond= PTHREAD_COND_INITIALIZER;
+static pthread_mutex_t readyMtx= PTHREAD_MUTEX_INITIALIZER;
+static pthread_t connectionChecker;
+static pthread_t outputPrinter;
+static pthread_t errPrinter;
+char* outbuff=NULL;
+char* errbuff=NULL;
+
+int goPlease=0; 
+int client_socket,lifeline_socket, output_socket,err_socket;
+static struct sockaddr_in server_address;
+
+static void sigint_handler(int signal){
+
+	acessVarMtx(&varMtx,&alive,0,0);
+	acessVarMtx(&varMtx,&goPlease,0,1);
+	pthread_cond_signal(&readyCond);
+	pthread_join(connectionChecker,NULL);
+	pthread_join(outputPrinter,NULL);
+	pthread_join(errPrinter,NULL);
+	if(outbuff){
+		free(outbuff);
+	}
+	if(errbuff){
+		free(errbuff);
+	}
+	close(client_socket);
+	close(lifeline_socket);
+	close(output_socket);
+	close(err_socket);
+	printf("cliente a fechar!!!\n");
+	exit(-1);
+
+}
+static void sigpipe_handler(int signal){
+
+	raise(SIGINT);
+	
+}
+static int64_t receiveServerOutput(int socket,char buff[],u_int64_t size,int secwait,int usecwait){
+                int iResult;
+                struct timeval tv;
+                fd_set rfds;
+                FD_ZERO(&rfds);
+                FD_SET(socket,&rfds);
+                tv.tv_sec=secwait;
+                tv.tv_usec=usecwait;
+                iResult=select(socket+1,&rfds,(fd_set*)0,(fd_set*)0,&tv);
+                if(iResult>0){
+                return recv(socket,buff,size,0);
+                }
+                return -1;
+}
+
+static void* getOutput(void* args){
+	outbuff=malloc(dataSize);
+	memset(outbuff,0,dataSize);
+	char buff[strlen(ping)];
+	while(acessVarMtx(&varMtx,&alive,0,-1)){
+	int numread=1;
+	while((numread=receiveServerOutput(output_socket,outbuff,dataSize,MAXTIMEOUTSECS,MAXTIMEOUTUSECS))>0){
+		printf("%s",outbuff);
+		send(output_socket,ping,strlen(ping),0);
+		memset(outbuff,0,dataSize);
+	}
+	}
+	
+
+}
+static void* getErr(void* args){
+	errbuff=malloc(dataSize);
+	memset(errbuff,0,dataSize);
+	acessVarMtx(&varMtx,&goPlease,1,0);
+	pthread_cond_signal(&readyCond);
+       	char buff[strlen(ping)];
+	while(acessVarMtx(&varMtx,&alive,0,-1)){
+	int numread=1;
+	while((numread=receiveServerOutput(err_socket,errbuff,dataSize,MAXTIMEOUTSECS,MAXTIMEOUTUSECS))>0){
+				fprintf(stderr,"%s",errbuff);
+				send(err_socket,ping,strlen(ping),0);
+				memset(errbuff,0,dataSize);
+			
+	}
+	
+	}
+	
+
+}
+static void initClient(int port, char* addr){
+
+	client_socket= socket(AF_INET,SOCK_STREAM,0);
+	if(client_socket==-1){
+		raise(SIGINT);
+	}
+	lifeline_socket= socket(AF_INET,SOCK_STREAM,0);
+	if(lifeline_socket==-1){
+		raise(SIGINT);
+	}
+	output_socket= socket(AF_INET,SOCK_STREAM,0);
+	if(output_socket==-1){
+		raise(SIGINT);
+	}
+	err_socket= socket(AF_INET,SOCK_STREAM,0);
+	if(err_socket==-1){
+		raise(SIGINT);
+	}
+	
+       long flags= fcntl(client_socket,F_GETFL);
+        flags |= O_NONBLOCK;
+        fcntl(client_socket,F_SETFD,flags);
+        
+	flags= fcntl(lifeline_socket,F_GETFL);
+        flags |= O_NONBLOCK;
+        fcntl(lifeline_socket,F_SETFD,flags);
+	
+	flags= fcntl(output_socket,F_GETFL);
+        flags |= O_NONBLOCK;
+        fcntl(output_socket,F_SETFD,flags);
+
+	flags= fcntl(err_socket,F_GETFL);
+        flags |= O_NONBLOCK;
+        fcntl(err_socket,F_SETFD,flags);
+
+	signal(SIGINT,sigint_handler);
+	signal(SIGPIPE,sigpipe_handler);
+	server_address.sin_family=AF_INET;
+	server_address.sin_addr.s_addr = inet_addr(addr);	
+	server_address.sin_port= htons(port);
+	
+
+}
+
+void tryConnect(int* socket){
+	int success=-1;
+	int numOfTries=MAXNUMBEROFTRIES;
+	
+	
+        while(success==-1&& numOfTries){
+                printf("Tentando conectar a %s (Tentativa %d)!!!!!!\n",inet_ntoa(server_address.sin_addr),-numOfTries+MAXNUMBEROFTRIES+1);		
+		success=connect(*socket,(struct sockaddr*)&server_address,sizeof(server_address));
+               int sockerr;
+		socklen_t slen=sizeof(sockerr);
+		getsockopt(*socket,SOL_SOCKET,SO_ERROR,(char*)&sockerr,&slen);
+		numOfTries--;
+		if(sockerr==EINPROGRESS){
+			
+			fprintf(stderr,"Erro normal:%s\n Erro Socket%s\nNumero socket: %d\n",strerror(errno),strerror(sockerr),*socket);
+			continue;
+
+		}
+		fd_set wfds;
+		FD_ZERO(&wfds);
+		FD_SET((*socket),&wfds);
+
+                struct timeval t;
+		t.tv_sec=MAXTIMEOUTCONS;
+		t.tv_usec=MAXTIMEOUTUCONS;
+		int iResult=select((*socket)+1,0,&wfds,0,&t);
+		
+		if(iResult>0&&!success&&((*socket)!=-1)){
+		//printf("Coneccao de %s!!!!!!\n",inet_ntoa(server_address.sin_addr));		
+			break;
+			
+		}
+		fprintf(stderr,"Não foi possivel: %s\n",strerror(errno));
+        }
+        if(!numOfTries){
+        printf("Não foi possivel conectar. Numero limite de tentativas (%d) atingido!!!\n",MAXNUMBEROFTRIES);
+        raise(SIGINT);
+        }
+
+}
+
+static void* areYouStillThere(void* args){
+	int pingLength=strlen(ping);
+	char buff[pingLength];
+	memset(buff,0,pingLength);
+	
+	while(acessVarMtx(&varMtx,&alive,0,-1)){
+
+                send(lifeline_socket,ping,pingLength,0);
+		int status=receiveServerOutput(lifeline_socket,buff,pingLength,MAXTIMEOUTPING,MAXTIMEOUTUPING);
+		if(status<0){
+			printf("Ops! You timed out.... Goodbye!\n");
+			raise(SIGINT);
+		}
+		
+		memset(buff,0,pingLength);
+
+        }
+
+}
+
+
+
+int main(int argc, char ** argv){
+
+	if(argc!=3){
+
+		printf("Utilizacao correta: arg1: ip do server.\narg2: porta\n");
+		exit(-1);
+	}
+	initClient(atoi(argv[1]),argv[2]);
+	tryConnect(&client_socket);
+	tryConnect(&lifeline_socket);
+	tryConnect(&output_socket);
+	tryConnect(&err_socket);
+	
+	char buff2[10]={0};
+	
+	receiveServerOutput(client_socket,buff2,10,MAXTIMEOUTCONS,MAXTIMEOUTUCONS);
+	sscanf(buff2,"%hu",&dataSize);
+	printf("%hu\n",dataSize);
+	//especificar socket;
+	send(client_socket,ping,strlen(ping),0);
+	
+	pthread_create(&connectionChecker,NULL,areYouStillThere,NULL);
+	pthread_create(&outputPrinter,NULL,getOutput,NULL);
+	pthread_create(&errPrinter,NULL,getErr,NULL);
+		pthread_mutex_lock(&readyMtx);
+		while(!acessVarMtx(&varMtx,&goPlease,0,-1)&&acessVarMtx(&varMtx,&alive,0,-1)){
+			pthread_cond_wait(&readyCond,&readyMtx);
+
+		}
+		
+		pthread_mutex_unlock(&readyMtx);
+		while(1){
+				char line[LINESIZE];
+				memset(line,0,LINESIZE);
+				printf("RemoteShell:> ");
+				fgets(line,LINESIZE,stdin);
+				line[strlen(line)-1]=0;
+				char buff[strlen(ping)+1];
+				int sent=send(client_socket,line,LINESIZE,0);
+				receiveServerOutput(client_socket,buff,sizeof(buff),MAXTIMEOUTCMD,MAXTIMEOUTUCMD);
+			
+			}
+		        raise(SIGINT);
+	
+}
+
